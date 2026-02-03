923cd7019dda1b20e9d45f3f5be33dae6200c218
Ilyes Zenati <zenailyes93@gmail.com>
2026-02-03 11:13:11 +0000

    Create Menu.h and we declare in it our 2 functions (afficherMenu and choixLog)

---
+++ Menu/Menu.h
@@ -0,0 +1,11 @@
+/**
+ * @file Menu.h
+ * @brief Déclaration des fonctions pour afficher le menu et traiter les choix de l'utilisateur.
+ * @author Ilyes
+ * @date 3 Février 2026
+ * @details
+ * Ce fichier contient le prototype des fonctions utilisées pour afficher le menu principal du programme de gestion des logs et pour traiter les choix de l'utilisateur.
+ */
+
+void afficherMenu();
+int choixLog(int nombreEntré);
