51eb252de186e022c3b5bde5692d95654e0e2b2a
Ilyes Zenati <zenailyes93@gmail.com>
2026-02-06 09:45:17 +0000

    Create function sshLog()

--- Logs/log.cpp
+++ Logs/log.cpp
@@ -14,7 +14,7 @@ Ce fichier contient les fonctions pour afficher les logs, comme les logs sudo, l
 #include "log.h"
 
 
-int sudoLog() { // Variable pour stocker le choix de l'utilisateur pour les logs sudo
+int sudoLog() { // Filtre les entrées de log contenant des requêtes sudo depuis le fichier /var/log/auth.log
     if (nombreEntré == 1) { // Si l'utilisateur choisit 1, on affiche les logs sudo
     std::ifstream file("/var/log/auth.log"); // Ouvre le fichier auth.log
     std::string line; // Variable pour stocker chaque ligne lue du fichier
@@ -33,3 +33,21 @@ int sudoLog() { // Variable pour stocker le choix de l'utilisateur pour les logs
     return 0;
 }
 
+int sshLog() { // Structure pour stocker les éléments concernant les log de connexion SSH
+    if (nombreEntré == 2) { // Si l'utilisateur choisit 2, on affiche les logs ssh
+    std::ifstream file("/var/log/syslog"); // Ouvre le fichier profile
+    std::string line; // Variable pour stocker chaque ligne lue du fichier
+    
+    if (file.is_open()) { // Vérifie si le fichier est ouvert correctement
+        while (std::getline(file, line)) { // Lit chaque ligne du fichier
+            if (line.find("ssh") != std::string::npos) { // Si la ligne contient "ssh", on l'affiche
+                std::cout << line << std::endl;
+            } 
+        }
+    file.close();
+    } else {
+        std::cout << "Impossible d'ouvrir le fichier." << std::endl;
+    }
+    return 0;
+}
+}
